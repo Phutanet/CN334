4f20a5341d3eb7b68ccbfdde6ed9cebbe4d8e0fb
nice <6210742463@student.tu.ac.th>
2022-01-19 19:09:31 +0700

    add cookie homework and new folder for CN334, SF332

---
+++ 6210742463-CN334/cookie.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<?php
+$cookie_name = "User";
+$cookie_value = "Siraphat Phetsakun";
+setcookie($cookie_name, $cookie_value, time() + (30 * 30), "/"); // 86400 = 1 day
+?>
+<html>
+<body>
+
+<?php
+if(!isset($_COOKIE[$cookie_name])) {
+     echo "Cookie named '" . $cookie_name . "' is not set!";
+} else {
+     echo  $cookie_name . " : " . $_COOKIE[$cookie_name];
+}
+?>
+
+</body>
+</html>
