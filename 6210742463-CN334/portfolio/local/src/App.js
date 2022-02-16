db7adcd2cfde567c8a39ef227becb194f29f3e77
kerkkerk12 <6210742356@student.tu.ac.th>
2022-02-16 19:31:59 +0700

    lost1

--- 6210742463-CN334/portfolio/local/src/App.js
+++ 6210742463-CN334/portfolio/local/src/App.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import './App.css';
 import React, {useState} from 'react';
 =======
@@ -6,6 +7,10 @@ import React from 'react';
 import Navbar from './components/navbar/Navbar';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 >>>>>>> parent of 189e93f (add login page and navbar responsive)
+=======
+import './App.css';
+import React, {useState} from 'react';
+>>>>>>> parent of a04011d (Revert "Merge branch 'main' of https://github.com/Phutanet/CN334")
 
 function App() {
 
@@ -20,17 +25,23 @@ function App() {
 
   return (
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of a04011d (Revert "Merge branch 'main' of https://github.com/Phutanet/CN334")
     <div>
       <button  onClick={onClickHello}>แสดงสวัสดี</button>
       <div>{hello}</div>
     </div>
   );
+<<<<<<< HEAD
 =======
     <>
     <Navbar />
     </>
   )
 >>>>>>> parent of 189e93f (add login page and navbar responsive)
+=======
+>>>>>>> parent of a04011d (Revert "Merge branch 'main' of https://github.com/Phutanet/CN334")
 }
 
 export default App;
