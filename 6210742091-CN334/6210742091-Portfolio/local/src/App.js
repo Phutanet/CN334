9ed41bfb378781671ab1d020932bc236924ae2e1
Phutanet <6210742091@student.tu.ac.th>
2022-02-20 23:06:00 +0700

    make CRUD system

--- 6210742091-CN334/6210742091-Portfolio/local/src/App.js
+++ 6210742091-CN334/6210742091-Portfolio/local/src/App.js
@@ -1,20 +1,22 @@
-import './App.css';
-import React, {useState} from 'react';
-import Navbar from './Components/Navbar';
-import Home from './Components/Home';
-import Contributions from './Components/Contributions';
-import Contact from './Components/Contact';
+import "./App.css";
+import React from "react";
+
+
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import Contributions from "./Components/Contributions";
+import Contact from "./Components/Contact";
+
 
 function App() {
-
-  return (
-    <div className='main-wallpaper'>
-      <Navbar/>
-      <Home/>
-      <Contributions/>
-      <Contact/>
-    </div>
-  );
+    return (
+        <div className="main-wallpaper">
+            <Navbar />
+            <Home />
+            <Contributions/>
+            <Contact />
+        </div>
+    );
 }
 
 export default App;
--- 6210742091-CN334/6210742091-Portfolio/portfolio/routes/api.php
+++ 6210742091-CN334/6210742091-Portfolio/portfolio/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/projects', [ProjectController::class, 'store']);
+Route::get('/projects', [ProjectController::class, 'index']);
+
+Route::get('/projects/{id}', [ProjectController::class, 'read']);
+Route::put('/projects/{id}', [ProjectController::class, 'edit']);
+Route::delete('/projects/{id}', [ProjectController::class, 'destroy']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
