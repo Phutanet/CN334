3bf6d637fdc8b95638a3e53125083a80bca16c6d
Siraphat <6210742463@student.tu.ac.th>
2022-03-14 13:58:14 +0700

    edit unit test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
-use App\Models\User;
 
 class ExampleTest extends TestCase
 {
@@ -15,16 +14,5 @@ class ExampleTest extends TestCase
     public function testBasicTest()
     {
         $this->assertTrue(true);
-    }
-
-    public function testUserCreation()
-    {
-        $user = new User([
-            'name' => "Test User",
-            'email' => "test@mail.com",
-            'password' => bcrypt("testpassword")
-        ]);
-
-        $this->assertEquals('Test User', $user->name);
     }
 }
