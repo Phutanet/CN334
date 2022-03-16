f12c0965003672995eb538f125871148dbb24406
Phutanet <6210742091@student.tu.ac.th>
2022-03-16 18:08:52 +0700

    make duplicateUser Test

---
+++ tests/Unit/duplicateUser.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class duplicateUser extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_user_duplication()
+    {
+        $user1 = User::make([
+            'name' => 'phutanet',
+            'email' => 'ice.test1@gmail.com'
+        ]);
+
+        $user2 = User::make([
+            'name' => 'phutanet',
+            'email' => 'ice.test2@gmail.com'
+        ]);
+        
+        $this->assertTrue($user1->email != $user2->email);
+    }
+}
