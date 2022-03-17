8ecc57895402bf7ccd1bac8d0eb9d7a99b1bc0ce
Siraphat <6210742463@student.tu.ac.th>
2022-03-17 14:54:37 +0700

    edit ci uat

--- tests/Browser/UAT/RegisterTest.php
+++ tests/Browser/UAT/RegisterTest.php
@@ -2,30 +2,19 @@
 
 namespace Tests\Browser\UAT;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
-use App\Models\User;
-
 class RegisterTest extends DuskTestCase
 {
-
-    use RefreshDatabase, WithFaker;
-
-    public function test_user_can_register()
+    public function test_user_can_go_to_register_page()
     {
         $this->browse(function ($browser) {
-            $browser->visit('/register')
-                    ->type('name', 'example')
-                    ->type('email', 'example@abc.com')
-                    ->type('password', 'examplepassword')
-                    ->type('password_confirmation', 'examplepassword')
-                    ->press('Register')
-                    ->assertPathIs('/dashboard');
+            $browser->visit('/')
+                    ->clickLink('Register')
+                    ->assertPathIs('/register');
         });
     }
 }
