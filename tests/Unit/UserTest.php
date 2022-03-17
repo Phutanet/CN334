d73f039dbc8afbe445b18595749019b8715ef25c
Siraphat <6210742463@student.tu.ac.th>
2022-03-17 16:35:53 +0700

    edit Unit/UserTest

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 
+use App\Models\User;
+
 class UserTest extends TestCase
 {
     
@@ -18,6 +20,21 @@ class UserTest extends TestCase
             Schema::hasColumns(
                 'users', ['id', 'name', 'email', 'email_verified_at', 'password','two_factor_secret','two_factor_recovery_codes', 'remember_token', 'current_team_id', 'profile_photo_path', 'created_at', 'updated_at']
             ));
+    }
+
+    public function test_user_duplication()
+    {
+        $user1 = User::make([
+            'name' => 'phutanet',
+            'email' => 'ice.test1@gmail.com'
+        ]);
+
+        $user2 = User::make([
+            'name' => 'phutanet',
+            'email' => 'ice.test2@gmail.com'
+        ]);
+        
+        $this->assertTrue($user1->email != $user2->email);
     }
     
 }
