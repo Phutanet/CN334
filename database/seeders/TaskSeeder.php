3ddfcbd9d1715c945f31d3cc3eacdaf30c342b7d
Phutanet <6210742091@student.tu.ac.th>
2022-03-23 10:55:45 +0700

    seederTest2

---
+++ database/seeders/TaskSeeder.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+use App\Models\Task;
+use App\Models\User;
+
+class TaskSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            "id" => 1,
+            "name" => "ice",
+            "email" => "ice@gmail.com",
+            "password" => "password",
+        ]);
+
+        User::create([
+            "id" => 2,
+            "name" => "nice",
+            "email" => "nice@gmail.com",
+            "password" => "password",
+        ]);
+
+        User::create([
+            "id" => 3,
+            "name" => "kerk",
+            "email" => "kerk@gmail.com",
+            "password" => "password",
+        ]);
+
+        //Create Task Seeder
+        Task::create([
+            "description" => "ice's task",
+            "user_id" => 1,
+        ]);
+
+        Task::create([
+            "description" => "nice's task",
+            "user_id" => 2,
+        ]);
+
+        Task::create([
+            "description" => "kerk's task",
+            "user_id" => 3,
+        ]);
+    }
+}
